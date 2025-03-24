a159619fb76f382d178b1800b5ca13be2a0ef508
knoori-code <knoori3@gmail.com>
2025-03-24 19:48:16 -0400

    Create encode function

--- Day 8 - Caesar Cipher/caesar_cipher_part1.py
+++ Day 8 - Caesar Cipher/caesar_cipher_part1.py
@@ -5,5 +5,27 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+# Take input message and shift each char by the shift amount
 def encrypt(original_text, shift_amount):
+    new_messsage = ''
+    for char in original_text:
+        if char not in alphabet:
+            new_messsage += char
+        else:
+            index_position = alphabet.index(char)
+            new_index = index_position + shift_amount
+            if new_index > 26:
+                while new_index > 26:
+                    new_index -= 26
+                new_index -= 1
+
+            if new_index < -26:
+                while new_index < -26:
+                    new_index = new_index + 26
+
+            new_messsage += alphabet[new_index]
+        
     
+    print(new_messsage)
+
+encrypt(original_text=text, shift_amount=2)
