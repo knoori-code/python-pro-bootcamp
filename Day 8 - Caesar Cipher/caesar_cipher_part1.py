f2a7ee7f41a062a037027e1dc354c3698b807acd
knoori-code <knoori3@gmail.com>
2025-03-29 19:16:07 -0400

    Move function declarations outside of while loop

--- Day 8 - Caesar Cipher/caesar_cipher_part1.py
+++ Day 8 - Caesar Cipher/caesar_cipher_part1.py
@@ -1,6 +1,42 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
+def encrypt(original_text, shift_amount):
+    encrypted_message = ''
+    for char in original_text:
+        if char not in alphabet:
+            encrypted_message += char
+        else:
+            original_index = alphabet.index(char)
+            new_index = original_index + shift_amount
+            new_index = new_index % len(alphabet)
+            encrypted_message += alphabet[new_index]
+
+    print(f"The new message is: {encrypted_message}")
+
+
+def decrypt(original_text, shift_amount):
+    decrypted_message = ''
+    for char in original_text:
+        if char not in alphabet:
+            decrypted_message += char
+        else:
+            original_index = alphabet.index(char)
+            new_index = original_index - shift_amount
+            new_index = new_index % len(alphabet)
+            decrypted_message += alphabet[new_index]
+
+    print(f"The decrypted message is: {decrypted_message}")
+
+
+def caesar(original_text, shift_amount, direction):
+        if direction == "encode":
+            encrypt(original_text, shift_amount)
+        else:
+            decrypt(original_text, shift_amount)
+
+
 continue_cipher = True
 
 while continue_cipher:
@@ -8,40 +44,6 @@ while continue_cipher:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-
-    # Take input message and shift each char by the shift amount
-    def encrypt(original_text, shift_amount):
-        encrypted_message = ''
-        for char in original_text:
-            if char not in alphabet:
-                encrypted_message += char
-            else:
-                original_index = alphabet.index(char)
-                new_index = original_index + shift_amount
-                new_index = new_index % len(alphabet)
-                encrypted_message += alphabet[new_index]
-
-        print(f"The new message is: {encrypted_message}")
-
-    def decrypt(original_text, shift_amount):
-        decrypted_message = ''
-        for char in original_text:
-            if char not in alphabet:
-                decrypted_message += char
-            else:
-                original_index = alphabet.index(char)
-                new_index = original_index - shift_amount
-                new_index = new_index % len(alphabet)
-                decrypted_message += alphabet[new_index]
-
-        print(f"The decrypted message is: {decrypted_message}")
-
-
-    def caesar(original_text, shift_amount, direction):
-        if direction == "encode":
-            encrypt(original_text, shift_amount)
-        else:
-            decrypt(original_text, shift_amount)
 
     caesar(original_text=text, shift_amount=shift, direction=direction)
         
