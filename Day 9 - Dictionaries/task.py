ed6e50ee453b11ded525661a6b01c2e812b3153e
knoori-code <knoori3@gmail.com>
2025-03-31 19:58:06 -0400

    Complete task.py and create nesting file

---
+++ Day 9 - Dictionaries/task.py
@@ -0,0 +1,8 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents it from running as expected",
+    "Function": "A piece of code that you can easily call over and over",
+    "Loop": "The action of doing something over and over again",
+}
+
+programming_dictionary["Expression"] = "A combination of a value, variable or operator"
+print(programming_dictionary)
