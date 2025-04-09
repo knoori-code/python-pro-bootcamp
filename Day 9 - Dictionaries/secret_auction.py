b37ff89dad059f5e8475eb13e79c3d4821d7aeba
knoori-code <knoori3@gmail.com>
2025-04-09 17:49:09 -0400

    Create dictionary and while loop

---
+++ Day 9 - Dictionaries/secret_auction.py
@@ -0,0 +1,12 @@
+continue_auction = True
+auction_dictionary = {}
+
+while continue_auction:
+
+    name = input("What is your name?: ")
+    bid = input("What is your bid?: $")
+
+    auction_dictionary[name] = bid
+
+
+
