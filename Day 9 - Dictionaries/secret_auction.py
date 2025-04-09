5744877a1d450e3b163ae1cdd941be78e04b841d
knoori-code <knoori3@gmail.com>
2025-04-09 18:02:34 -0400

    Complete auction program

--- Day 9 - Dictionaries/secret_auction.py
+++ Day 9 - Dictionaries/secret_auction.py
@@ -4,7 +4,7 @@ auction_dictionary = {}
 while continue_auction:
 
     name = input("What is your name?: ")
-    bid = input("What is your bid?: $")
+    bid = int(input("What is your bid?: $"))
 
     auction_dictionary[name] = bid
 
@@ -12,5 +12,10 @@ while continue_auction:
     if continue_response == 'no':
         continue_auction = False
 
+max_bid = 0
 for key in auction_dictionary:
-    
+    if auction_dictionary[key] > max_bid:
+        max_bid = auction_dictionary[key]
+        name = key
+
+print(f"The winner is {name} with a bid of ${max_bid}!")
