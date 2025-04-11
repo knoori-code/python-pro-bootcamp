0932b2d7759b66c29bc68ec6c099d6754995b222
knoori-code <knoori3@gmail.com>
2025-04-10 20:38:01 -0400

    Complete leap year calculator

---
+++ Day 10 - Functions With Outputs/leap_year.py
@@ -0,0 +1,13 @@
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    
+    if year % 100 == 0:
+        return False
+    
+    if year % 4 == 0:
+        return True
+    
+    return False
+
+print(is_leap_year(2100))
