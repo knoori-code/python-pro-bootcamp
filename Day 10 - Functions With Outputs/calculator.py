553ae235b85f9fcf4c6888610446a8820c22f4cb
knoori-code <knoori3@gmail.com>
2025-04-12 10:49:28 -0400

    Ask for first number and operation

--- Day 10 - Functions With Outputs/calculator.py
+++ Day 10 - Functions With Outputs/calculator.py
@@ -16,3 +16,19 @@ operations_dictionary = {
     "*": multiply,
     "/": divide
 }
+
+# multiply 4 * 8
+# print(operations_dictionary["*"](4, 8))
+
+first_number = int(input("What's the first number?: "))
+
+operation_list = ["+", "-", "*", "/"]
+
+for operation in operation_list:
+    print(operation)
+
+chosen_operation = input("Pick an operation: ")
+
+
+
+
