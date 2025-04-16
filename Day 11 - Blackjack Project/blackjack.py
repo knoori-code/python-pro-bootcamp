ceae292fc1391cb0d74a2b0b7acdad7f68308cd6
knoori-code <knoori3@gmail.com>
2025-04-15 21:27:59 -0400

    Create random card generator function

--- Day 11 - Blackjack Project/blackjack.py
+++ Day 11 - Blackjack Project/blackjack.py
@@ -1,5 +1,13 @@
+import random
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 begin_game = input("Do you want to play a game of blackjack? type 'y' or 'no': ")
 
-# Deal cards to player and computer
+player_cards = []
+computer_cards = []
+
+def generate_random_card():
+    index = random.randint(0, 12)
+    random_card = cards[index]
+    return random_card
