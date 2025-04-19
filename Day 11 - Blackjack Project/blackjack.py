eb926bc24eedaef370f015b6783080cd3e05cb00
knoori-code <knoori3@gmail.com>
2025-04-18 21:02:50 -0400

    Create while loop to begin game

--- Day 11 - Blackjack Project/blackjack.py
+++ Day 11 - Blackjack Project/blackjack.py
@@ -2,9 +2,6 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-# begin_game = input("Do you want to play a game of blackjack? type 'y' or 'no': ")
-
-
 def check_score(card_list):
     total = sum(card_list)
     return total
@@ -14,20 +11,23 @@ def generate_random_card():
     random_card = cards[index]
     return random_card
 
-question = input("Do you want to play a game of Blackjack? Type 'y' or 'n':").lower()
-player_cards = []
-computer_cards = []
+start_game = True
 
-for i in range(2):
-    player_cards.append(generate_random_card())
-    computer_cards.append(generate_random_card())
+while start_game:
+    question = input("Do you want to play a game of Blackjack? Type 'y' or 'n':").lower()
+    player_cards = []
+    computer_cards = []
 
-computer_first_card = computer_cards[0]
+    for i in range(2):
+        player_cards.append(generate_random_card())
+        computer_cards.append(generate_random_card())
 
-print(f"Your cards: {player_cards}, current score: {check_score(player_cards)}")
-print(f"Computer's first card: {computer_first_card}")
+    computer_first_card = computer_cards[0]
 
-another_card_choice = input("Type 'y' to get another card, type 'n' to pass: ")
+    print(f"Your cards: {player_cards}, current score: {check_score(player_cards)}")
+    print(f"Computer's first card: {computer_first_card}")
 
-if another_card_choice == 'y':
-    player_cards.append(generate_random_card())
+    another_card_choice = input("Type 'y' to get another card, type 'n' to pass: ")
+
+    if another_card_choice == 'y':
+        player_cards.append(generate_random_card())
