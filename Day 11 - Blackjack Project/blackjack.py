481727f77c5157d8a4f333c06c7255685f5df00b
knoori-code <knoori3@gmail.com>
2025-04-18 20:52:57 -0400

    Move where cards are generated

--- Day 11 - Blackjack Project/blackjack.py
+++ Day 11 - Blackjack Project/blackjack.py
@@ -4,8 +4,6 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 # begin_game = input("Do you want to play a game of blackjack? type 'y' or 'no': ")
 
-player_cards = []
-computer_cards = []
 
 def check_score(card_list):
     total = sum(card_list)
@@ -16,16 +14,16 @@ def generate_random_card():
     random_card = cards[index]
     return random_card
 
-# generate 2 cards for the player and computer
+question = input("Do you want to play a game of Blackjack? Type 'y' or 'n':").lower()
+player_cards = []
+computer_cards = []
+
 for i in range(2):
     player_cards.append(generate_random_card())
     computer_cards.append(generate_random_card())
 
 computer_first_card = computer_cards[0]
 
-# print(player_cards, computer_cards)
-
-question = input("Do you want to play a game of Blackjack? Type 'y' or 'n':").lower()
 print(f"Your cards: {player_cards}, current score: {check_score(player_cards)}")
 print(f"Computer's first card: {computer_first_card}")
 
