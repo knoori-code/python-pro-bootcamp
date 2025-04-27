468c1754d476188de7d0d50dc052f4b16b5186a0
knoori-code <knoori3@gmail.com>
2025-04-27 16:16:11 -0400

    Move all function definitions to top of file

--- Day 11 - Blackjack Project/blackjack.py
+++ Day 11 - Blackjack Project/blackjack.py
@@ -1,26 +1,13 @@
 import random
 import art
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-play_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-
-logo = art.logo
-print(logo)
-
-player_cards = []
-computer_cards = []
-
 # Add a new card to hand
 def generate_new_card(whose_cards):
     random_index = random.randint(0, 12)
     whose_cards.append(cards[random_index])
 
-# Generate 2 random cards for computer and player
-for i in range(2):
-    generate_new_card(player_cards)
-    generate_new_card(computer_cards)
 
+# Generate 2 random cards for computer and player
 def complete_computer_hand(hand):
     score = sum(hand)
     while score < 17:
@@ -31,9 +18,25 @@ def complete_computer_hand(hand):
             hand[index_for_11] = 1
             score = sum(hand)
 
+
 # Function to tally and show final hands and scores
 def final_game_score(who, hand, total):
     print(f"{who} final hand: {hand}, final score: {total}")
+
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+play_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+logo = art.logo
+print(logo)
+
+player_cards = []
+computer_cards = []
+
+for i in range(2):
+    generate_new_card(player_cards)
+    generate_new_card(computer_cards)
 
 player_score = sum(player_cards)
 computer_score = sum(computer_cards)
