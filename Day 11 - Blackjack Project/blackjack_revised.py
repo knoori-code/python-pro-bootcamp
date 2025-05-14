7d38759c00132ef7825a9c6b4c6cdbeaf530763c
knoori-code <knoori3@gmail.com>
2025-05-14 17:15:08 -0400

    Create hand variables for both players

--- Day 11 - Blackjack Project/blackjack_revised.py
+++ Day 11 - Blackjack Project/blackjack_revised.py
@@ -3,7 +3,13 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-play_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n'").lower()
+def blackjack(): 
+    play_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n'").lower()
+    player_hand = []
+    computer_hand = []
 
-if play_game == 'y':
-    print(art.logo)
+    if play_game == 'y':
+        print(art.logo)
+
+        for i in range(2):
+            # Add cards to player and computer cards
