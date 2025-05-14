9a7ae2dc8318f2fe40d5bb54ae92c0ecca11bc20
knoori-code <knoori3@gmail.com>
2025-05-14 17:25:18 -0400

    Generate 2 cards for each player

--- Day 11 - Blackjack Project/blackjack_revised.py
+++ Day 11 - Blackjack Project/blackjack_revised.py
@@ -3,6 +3,10 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+def generate_random_card(hand):
+    random_index = random.randint(0, 12)
+    hand.append(cards[random_index])
+
 def blackjack(): 
     play_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n'").lower()
     player_hand = []
@@ -11,5 +15,7 @@ def blackjack():
     if play_game == 'y':
         print(art.logo)
 
+        # Generate 2 cards for each player
         for i in range(2):
-            # Add cards to player and computer cards
+            generate_random_card(player_hand)
+            generate_random_card(computer_hand)
