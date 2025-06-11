f86a3ec4d48b531d1bcf0a1c086c445beca8e1fd
knoori-code <knoori3@gmail.com>
2025-06-11 19:27:35 -0400

    Add else statement if player does not want card

--- Day 11 - Blackjack Project/blackjack_revised.py
+++ Day 11 - Blackjack Project/blackjack_revised.py
@@ -84,8 +84,8 @@ def blackjack():
                     index_position = player_hand.index(11)
                     player_hand[index_position] = 1
 
-
-            # If player score < 21, ask if player wants another card. If not, tally computer score and compare scores
+            else:
+                
 
         
 
