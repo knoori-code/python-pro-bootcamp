436fc72c7d2a5f8b38c0ae7e86a6e0a441bb73d1
knoori-code <knoori3@gmail.com>
2025-07-06 11:11:03 -0400

    Define variable with attempts depending on players input

--- Day 12 - Scope and Number Guessing Game/guessing_game.py
+++ Day 12 - Scope and Number Guessing Game/guessing_game.py
@@ -1,9 +1,16 @@
 print("Welcome to the Number Guessing Game!")
-print("I'm thinking of anumber between 1 and 100.")
+print("I'm thinking of a number between 1 and 100.")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
 attempts_dict = {
     "easy": 10,
     "hard": 5
 }
+
+if difficulty == 'easy':
+    attempts = attempts_dict["easy"]
+else:
+    attempts = attempts_dict["hard"]
+
+
 
