1781ba40db904046eff8e68a8b95193cb49cc665
knoori-code <knoori3@gmail.com>
2025-07-06 11:54:26 -0400

    Use while loop to check if player wants to play again

--- Day 12 - Scope and Number Guessing Game/guessing_game.py
+++ Day 12 - Scope and Number Guessing Game/guessing_game.py
@@ -1,36 +1,40 @@
 import random
 
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-difficulty = (input("Choose a difficulty. Type 'easy' or 'hard': "))
-number = random.randint(1, 100)
-print(f"The secret number is {number}")
+def number_guessing_game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    difficulty = (input("Choose a difficulty. Type 'easy' or 'hard': "))
+    number = random.randint(1, 100)
+    print(f"The secret number is {number}")
 
-attempts_dict = {
-    "easy": 10,
-    "hard": 5
-}
+    attempts_dict = {
+        "easy": 10,
+        "hard": 5
+    }
 
-if difficulty == 'easy':
-    attempts = attempts_dict["easy"]
-else:
-    attempts = attempts_dict["hard"]
-
-continue_guessing = True
-while continue_guessing:
-    print(f"You have {attempts} attempts remaining to guess the number.")
-    guess = int(input("Make a guess: "))
-
-    if guess > number:
-        print("Too high.\nGuess again.")
-        attempts -= 1
-    elif guess < number:
-        print(f"Too low.\nGuess again.")
-        attempts -= 1
+    if difficulty == 'easy':
+        attempts = attempts_dict["easy"]
     else:
-        print(f"You got it! The answer was {number}.")
-        continue_guessing = False
+        attempts = attempts_dict["hard"]
 
-    if attempts == 0:
-        print(f"You've run out of guesses. Game over.")
-        continue_guessing = False
+    continue_guessing = True
+    while continue_guessing:
+        print(f"You have {attempts} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+
+        if guess > number:
+            print("Too high.\nGuess again.")
+            attempts -= 1
+        elif guess < number:
+            print(f"Too low.\nGuess again.")
+            attempts -= 1
+        else:
+            print(f"You got it! The answer was {number}.")
+            continue_guessing = False
+
+        if attempts == 0:
+            print(f"You've run out of guesses. Game over.")
+            continue_guessing = False
+
+while input(f"Would you like to play the Number Guessing Game? ").lower() == 'yes':
+    number_guessing_game()
