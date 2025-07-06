13428fa198d4e254c30e0e0e25bafd7740fb7595
knoori-code <knoori3@gmail.com>
2025-07-06 11:04:46 -0400

    Add print statements and create attempts dictionary

---
+++ Day 12 - Scope and Number Guessing Game/guessing_game.py
@@ -0,0 +1,9 @@
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of anumber between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+attempts_dict = {
+    "easy": 10,
+    "hard": 5
+}
+
