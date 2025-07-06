346767abc19d984d11a65fbda41f9cd0766f4bec
knoori-code <knoori3@gmail.com>
2025-07-06 11:24:10 -0400

    Check if guess is equal, greater or smaller than number to guess

--- Day 12 - Scope and Number Guessing Game/guessing_game.py
+++ Day 12 - Scope and Number Guessing Game/guessing_game.py
@@ -1,6 +1,9 @@
+import random
+
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+number = random.randint(1, 100)
 
 attempts_dict = {
     "easy": 10,
@@ -12,5 +15,18 @@ if difficulty == 'easy':
 else:
     attempts = attempts_dict["hard"]
 
+print(f"You have {attempts} attempts remaining to guess the number.")
 
+guess = input("Make a guess: ")
 
+if guess > number:
+    print("Too high.\nGuess again.")
+    attempts -= 1
+    print(f"You have {attempts} attempts remaining to guess the number.")
+elif guess < number:
+    print(f"Too low.\nGuess again.")
+    attempts -= 1
+    print(f"You have {attempts} attempts remaining to guess the number.")
+else:
+    print("You got it! The answer was {number}.")
+    
