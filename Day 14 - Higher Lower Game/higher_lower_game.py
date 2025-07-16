2cdb49546225daae3d6a7094624f47a685311749
knoori-code <knoori3@gmail.com>
2025-07-16 19:29:09 -0400

    Change variable names

--- Day 14 - Higher Lower Game/higher_lower_game.py
+++ Day 14 - Higher Lower Game/higher_lower_game.py
@@ -3,10 +3,11 @@ import art
 from game_data import data
 
 # Choose 2 items randomly from the data list to compare
-item1 = random.choice(data)
-item2 = random.choice(data)
+choice_a = random.choice(data)
+choice_b = random.choice(data)
 
 # Display both with "vs"" and game logo
+
 
 # Compare follower counts; if player is wrong, game over; if player is right, add 1 to score, choose new dictionary item to compare with correct 
 # item
