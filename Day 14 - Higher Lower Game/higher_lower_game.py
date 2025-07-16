91317a19fcadd042791cad61bbc49ca5abb205d7
knoori-code <knoori3@gmail.com>
2025-07-16 19:22:31 -0400

    Choose 2 random elements from list to compare

--- Day 14 - Higher Lower Game/higher_lower_game.py
+++ Day 14 - Higher Lower Game/higher_lower_game.py
@@ -1,10 +1,12 @@
+import random
 import art
+from game_data import data
 
-# Display game logo
+# Choose 2 items randomly from the data list to compare
+item1 = random.choice(data)
+item2 = random.choice(data)
 
-# Choose 2 items randomly from the dictionary to compare
-
-# Display both with vs. logo between them
+# Display both with "vs"" and game logo
 
 # Compare follower counts; if player is wrong, game over; if player is right, add 1 to score, choose new dictionary item to compare with correct 
 # item
