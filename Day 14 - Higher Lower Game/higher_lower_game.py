d91e22ee919d1c898ca2c187d814eb2920cc8eed
knoori-code <knoori3@gmail.com>
2025-07-16 19:11:35 -0400

    Add more to do comments

--- Day 14 - Higher Lower Game/higher_lower_game.py
+++ Day 14 - Higher Lower Game/higher_lower_game.py
@@ -4,5 +4,9 @@ import art
 
 # Choose 2 items randomly from the dictionary to compare
 
-# Display both for player with vs. logo between them
+# Display both with vs. logo between them
 
+# Compare follower counts; if player is wrong, game over; if player is right, add 1 to score, choose new dictionary item to compare with correct 
+# item
+
+# Compare both items
