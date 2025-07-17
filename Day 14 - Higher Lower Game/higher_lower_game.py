e5fdb967237bb1a2fe8f6ba92dfab9afb0990414
knoori-code <knoori3@gmail.com>
2025-07-16 20:02:23 -0400

    Determine if player chooses correct item and add to score or end game depending on result

--- Day 14 - Higher Lower Game/higher_lower_game.py
+++ Day 14 - Higher Lower Game/higher_lower_game.py
@@ -12,10 +12,24 @@ print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice
 print(art.versus)
 print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
 
+score = 0
 # Compare follower counts; if player is wrong, game over; if player is right, add 1 to score, item B becomes A, find new B
 player_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
 
 follower_count_a = choice_a['follower_count']
 follower_count_b = choice_b['follower_count']
+
+if player_choice == 'a' and follower_count_a > follower_count_b:
+    score += 1
+    choice_a = choice_b
+elif player_choice == 'a' and follower_count_a < follower_count_b:
+    score = 0
+    print("Sorry, that's wrong. Final score is {score}")
+elif player_choice == 'b' and follower_count_b > follower_count_a:
+    score += 1
+    choice_a = choice_b
+elif player_choice == 'b' and follower_count_b < follower_count_a:
+    print("Sorry, that's wrong. Final score is {score}")
+    
 
 # Compare both items again
