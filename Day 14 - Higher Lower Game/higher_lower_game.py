00fda82d2ae177349168aab06303d7a53d99b731
knoori-code <knoori3@gmail.com>
2025-07-21 14:31:55 -0400

    Complete game

--- Day 14 - Higher Lower Game/higher_lower_game.py
+++ Day 14 - Higher Lower Game/higher_lower_game.py
@@ -9,37 +9,42 @@ def check_answer(choice, followers_a, followers_b, score):
     elif choice == 'b' and followers_b > followers_a:
         score += 1
     else:
+        print("\n" * 5)
+        print(art.logo)
+        print(f"Sorry, that's wrong. Final score is {score}")
         score = 0
-        print("Sorry, that's wrong. Final score is 0")
     return score
 
 
-print(art.logo)
-
-# Choose 2 items randomly from the data list to compare
-choice_a = random.choice(data)
-choice_b = random.choice(data)
-
-follower_count_a = choice_a['follower_count']
-follower_count_b = choice_b['follower_count']
-
-# Display both with "vs"" and game logo
-print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}.")
-print(art.versus)
-print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
-
-score = 0
-
-# Compare follower counts; if player is wrong, game over; if player is right, add 1 to score, item B becomes A, find new B
-player_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
-score = check_answer(player_choice, follower_count_a, follower_count_b, score)
-
-if score != 0:
-    choice_a = choice_b
+def higher_lower_game():
+    score = 0
+    # Choose 2 items randomly from the data list to compare
+    choice_a = random.choice(data)
     choice_b = random.choice(data)
 
-# To clear console
-# print("\n" * 100)
-    
+    guessed_correct = True
 
-# Compare both items again
+    while guessed_correct:
+        print(art.logo)
+        follower_count_a = choice_a['follower_count']
+        follower_count_b = choice_b['follower_count']
+
+        # Display both with "vs"" and game logo
+        if score > 0:
+            print(f"You're right! Current score: {score}")
+        print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}.")
+        print(art.versus)
+        print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
+
+        # Compare follower counts; if player is wrong, game over; if player is right, add 1 to score, item B becomes A, find new B
+        player_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+        score = check_answer(player_choice, follower_count_a, follower_count_b, score)
+
+        if score > 0:
+            choice_a = choice_b
+            choice_b = random.choice(data)
+        else:
+            guessed_correct = False
+
+
+higher_lower_game()
